ff04db76c189b5dc34ad9f7c07803c87f8aed428
Alexander Pepper <alexander.pepper@infopark.de>
2019-03-21 14:44:12 +0100

    Proof-Of-Concept for TickListWidget.
    Please note, that TickListItemWidget can currently not be converted over to styled-components, since "styled(Scrivito.WidgetTag)" is currently broken.

--- src/Widgets/TickListWidget/TickListWidgetComponent.js
+++ src/Widgets/TickListWidget/TickListWidgetComponent.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
+import styled from "styled-components";
+import { clearFix } from "polished";
 
 Scrivito.provideComponent("TickListWidget", ({ widget }) => (
-  <Scrivito.ContentTag
-    tag="ul"
-    className="tick-list"
-    content={widget}
-    attribute="items"
-  />
+  <StyledUl tag="ul" className="tick-list" content={widget} attribute="items" />
 ));
+
+const StyledUl = styled(Scrivito.ContentTag)`
+  list-style-type: none;
+  padding-left: 15px;
+  ${clearFix()}
+`;
