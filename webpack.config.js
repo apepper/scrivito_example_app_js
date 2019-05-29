c9155d5714c5dd46612a03d8e43cc7e3956ebfa2
Alexander Pepper <alexander.pepper@infopark.de>
2019-05-29 12:52:23 +0200

    Use continually build npm package of scrivito.
    How to:
    
    > npm install            # bootstrap - every now and then
    > npm start              # UI dev server @ port 8090 and Live-update of your scrivito/NPM
    
    > npm install            # syncs your scrivito with the remote one
    > npm start
    
    Installed using:
    > npm uninstall scrivito && npm install --save-dev ../rails_connector/js/build/npm_scrivito
    
    [skip ci]

--- webpack.config.js
+++ webpack.config.js
@@ -121,7 +121,7 @@ function webpackConfig(env = {}) {
     resolve: {
       extensions: [".js"],
       modules: ["node_modules"],
-      symlinks: false,
+      symlinks: true,
     },
     devServer: {
       port: 8080,
@@ -204,6 +204,11 @@ function devServerCspHeader() {
 
   // allow ws: for webpack hot code reloading
   directives["default-src"].push("ws:");
+
+  // Add custom rules for onDevSdk
+  directives["default-src"].push("http:");
+  directives["script-src"].push("http:");
+  directives["style-src"].push("http:");
 
   return builder({ directives });
 }
