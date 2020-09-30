fff4a284ab415f7cd2afdd40ca42786ce185f832
Alexander Pepper <alexander.pepper@infopark.de>
2020-09-30 08:27:27 +0200

    Added multisite config
    [skip ci]

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,6 +1,17 @@
 import * as Scrivito from "scrivito";
 
 export function configureScrivito(options) {
+  const baseUrls = {
+    default: "http://localhost:8080/default",
+
+    berlin: "http://localhost:8080/berlin",
+    hamburg: "http://localhost:8080/hamburg",
+    munich: "http://localhost:8080/munich",
+    cologne: "http://localhost:8080/cologne",
+    frankfurt: "http://localhost:8080/frankfurt",
+    stuttgart: "http://localhost:8080/stuttgart",
+  };
+
   const config = {
     adoptUi: "http://localhost:8090",
     autoConvertAttributes: true,
@@ -10,12 +21,22 @@ export function configureScrivito(options) {
     tenant: process.env.SCRIVITO_TENANT,
     unstable: {
       assetUrlBase: "http://localhost:8091",
+      getSiteIdForObj: () => "intranet",
       trustedUiOrigins: [
         "http://localhost:8090",
         "http://127.0.0.1:8090",
         "https://*.scrivito-ui.pages.dev",
       ],
     },
+    baseUrlForSite: (siteId) => baseUrls[siteId],
+    siteForUrl: (url) =>
+      Object.keys(baseUrls)
+        .map((siteId) =>
+          url.startsWith(baseUrls[siteId])
+            ? { siteId, baseUrl: baseUrls[siteId] }
+            : undefined
+        )
+        .filter((result) => result)[0],
   };
 
   if (process.env.SCRIVITO_ORIGIN) config.origin = process.env.SCRIVITO_ORIGIN;
