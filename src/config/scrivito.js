65a88d56020e3be8118c93b57d2e0d6b5d777088
Alexander Pepper <alexander.pepper@infopark.de>
2021-01-28 14:24:54 +0100

    Configured multisite with en, de and pl.

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,6 +1,25 @@
 import * as Scrivito from "scrivito";
 
-const config = { tenant: process.env.SCRIVITO_TENANT, adoptUi: true };
+const baseUrls = {
+  default: "http://localhost:8080/en",
+
+  german: "http://localhost:8080/de",
+  polish: "http://localhost:8080/pl",
+};
+
+const config = {
+  tenant: process.env.SCRIVITO_TENANT,
+  adoptUi: true,
+  baseUrlForSite: (siteId) => baseUrls[siteId],
+  siteForUrl: (url) =>
+    Object.keys(baseUrls)
+      .map((siteId) =>
+        url.startsWith(baseUrls[siteId])
+          ? { siteId, baseUrl: baseUrls[siteId] }
+          : undefined
+      )
+      .filter((result) => result)[0],
+};
 
 if (process.env.SCRIVITO_ORIGIN) {
   config.origin = process.env.SCRIVITO_ORIGIN;
