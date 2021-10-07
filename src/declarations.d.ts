2451b27d27ad5458bc4bd48343f5e817c20bb7a0
Alexander Pepper <alexander.pepper@justrelate.com>
2021-10-07 12:11:36 +0200

    Typescript: Allow to import files via url-loader

---
+++ src/declarations.d.ts
@@ -0,0 +1,6 @@
+// Added, so that url-loader imports are also valid in typescript.
+// See https://github.com/Microsoft/TypeScript-React-Starter/issues/12#issuecomment-326370098
+declare module "*.gif";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
