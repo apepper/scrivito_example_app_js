9adf99684748e4d59db29db902660f569fa8c37b
Alexander Pepper <alexander.pepper@justrelate.com>
2021-11-04 13:42:30 +0100

    FormContainerWidget generated a random 32 char id on second render

--- src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
+++ src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
@@ -1,10 +1,22 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
+import { toHex } from "@aws-sdk/util-hex-encoding";
 
 import "./FormContainerWidget.scss";
 
-Scrivito.provideComponent("FormContainerWidget", ({ widget }) => (
-  <form className="form-container-widget" method="post">
-    <Scrivito.ContentTag content={widget} attribute="content" />
-  </form>
-));
+Scrivito.provideComponent("FormContainerWidget", ({ widget }) => {
+  const [formId] = React.useState(() => random32Hex());
+
+  return (
+    <form className="form-container-widget" method="post">
+      <input type="hidden" name="id" value={formId} />
+      <Scrivito.ContentTag content={widget} attribute="content" />
+    </form>
+  );
+});
+
+function random32Hex() {
+  const array = new Uint8Array(16);
+  crypto.getRandomValues(array);
+  return toHex(array);
+}
