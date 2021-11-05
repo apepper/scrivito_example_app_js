dea39610ab5b8e845bf60a4ee936b87b475e2198
Alexander Pepper <alexander.pepper@justrelate.com>
2021-11-05 11:22:49 +0100

    FormContainerWidget now has basic "submit" logic - without actual submit

--- src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
+++ src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
@@ -6,17 +6,50 @@ import "./FormContainerWidget.scss";
 
 Scrivito.provideComponent("FormContainerWidget", ({ widget }) => {
   const [formId] = React.useState(() => random32Hex());
+  const [submission, setSubmission] = React.useState("not submitted");
+
+  if (submission === "submitting") {
+    return <div className="form-container-widget">🛰️ Submitting...</div>;
+  }
+
+  if (submission === "success") {
+    return (
+      <div className="form-container-widget">🗳️ Successfully submitted.</div>
+    );
+  }
 
   return (
-    <form className="form-container-widget" method="post">
-      <input type="hidden" name="id" value={formId} />
-      <Scrivito.ContentTag content={widget} attribute="content" />
-    </form>
+    <div className="form-container-widget">
+      <form method="post" onSubmit={onSubmit}>
+        <input type="hidden" name="id" value={formId} />
+        <Scrivito.ContentTag content={widget} attribute="content" />
+      </form>
+    </div>
   );
+
+  async function onSubmit(element) {
+    element.preventDefault();
+
+    setSubmission("submitting");
+    await submit(extractFormData(element.target));
+    setSubmission("success");
+  }
 });
 
 function random32Hex() {
   const array = new Uint8Array(16);
   crypto.getRandomValues(array);
   return toHex(array);
+}
+
+function extractFormData(formElement) {
+  // TODO: If IE11 support is needed, something like https://www.npmjs.com/package/formdata-polyfill should be used
+  const formData = new FormData(formElement);
+  return Object.fromEntries(formData);
+}
+
+async function submit(formProps) {
+  // TODO: Implement actual submission
+  console.log("submitting", formProps);
+  return new Promise((resolve) => setTimeout(resolve, 3000));
 }
