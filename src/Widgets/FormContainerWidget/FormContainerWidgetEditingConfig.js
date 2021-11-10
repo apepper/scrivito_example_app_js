22c61db003813ec8df595bef69c14ab83af91631
Alexander Pepper <alexander.pepper@justrelate.com>
2021-11-10 11:37:29 +0100

    Added reasonable initial content to FormContainerWidget

--- src/Widgets/FormContainerWidget/FormContainerWidgetEditingConfig.js
+++ src/Widgets/FormContainerWidget/FormContainerWidgetEditingConfig.js
@@ -1,5 +1,8 @@
 import * as Scrivito from "scrivito";
 import formContainerWidgetIcon from "../../assets/images/form_container_widget.svg";
+import FormCheckboxWidget from "../FormCheckboxWidget/FormCheckboxWidgetClass";
+import FormSubmitButtonWidget from "../FormSubmitButtonWidget/FormSubmitButtonWidgetClass";
+import FormTextInputWidget from "../FormTextInputWidget/FormTextInputWidgetClass";
 
 import { random32CharHex } from "./random32CharHex";
 
@@ -22,6 +25,29 @@ Scrivito.provideEditingConfig("FormContainerWidget", {
     formId: () => random32CharHex(),
     submittingMessage: "Submitting...",
     submittedMessage: "Successfully submitted.",
+    content: () => [
+      new FormTextInputWidget({
+        type: "name",
+        label: "Name",
+        placeholder: "Your name",
+        required: false,
+      }),
+      new FormTextInputWidget({
+        type: "email",
+        label: "Email",
+        placeholder: "Your email",
+        required: true,
+      }),
+      new FormTextInputWidget({
+        type: "custom_text_area",
+        customFieldName: "message",
+        label: "Message",
+        placeholder: "Your message",
+        required: true,
+      }),
+      new FormCheckboxWidget(),
+      new FormSubmitButtonWidget(),
+    ],
   },
   properties: ["formId", "submittingMessage", "submittedMessage"],
 });
